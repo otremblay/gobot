c3ac20037a7e7c592a1b3a57e554abda2d935db8
Dany Patrie <dany.patrie@gmail.com>
2014-09-18 22:05:47 -0400

    Added troll plugin :)

---
+++ troll.go
@@ -0,0 +1,39 @@
+package seedbotplugin
+
+import (
+	"github.com/seedboxtech/xmppbot"
+	"strings"
+)
+
+const (
+	troll = `
+░░░░▄▄▄▄▀▀▀▀▀▀▀▀▄▄▄▄▄▄
+░░░░█░░░░▒▒▒▒▒▒▒▒▒▒▒▒░░▀▀▄
+░░░█░░░▒▒▒▒▒▒░░░░░░░░▒▒▒░░█
+░░█░░░░░░▄██▀▄▄░░░░░▄▄▄░░░█
+░▀▒▄▄▄▒░█▀▀▀▀▄▄█░░░██▄▄█░░░█
+█▒█▒▄░▀▄▄▄▀░░░░░░░░█░░░▒▒▒▒▒█
+█▒█░█▀▄▄░░░░░█▀░░░░▀▄░░▄▀▀▀▄▒█
+░█▀▄░█▄░█▀▄▄░▀░▀▀░▄▄▀░░░░█░░█
+░░█░░▀▄▀█▄▄░█▀▀▀▄▄▄▄▀▀█▀██░█
+░░░█░░██░░▀█▄▄▄█▄▄█▄████░█
+░░░░█░░░▀▀▄░█░░░█░███████░█
+░░░░░▀▄░░░▀▀▄▄▄█▄█▄█▄█▄▀░░█
+░░░░░░░▀▄▄░▒▒▒▒░░░░░░░░░░█
+░░░░░░░░░░▀▀▄▄░▒▒▒▒▒▒▒▒▒▒░█
+░░░░░░░░░░░░░░▀▄▄▄▄▄░░░░░█
+`
+)
+
+type Troll struct{}
+
+func (p Troll) Name() string {
+	return "Troll v1.0"
+}
+
+func (p Troll) Execute(msg xmppbot.Message, bot xmppbot.Bot) error {
+	if strings.Contains(msg.Body(), "troll") {
+		bot.Send(troll)
+	}
+	return nil
+}
