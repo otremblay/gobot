fe2999cc4670675e0b546900eec714c790e1b53b
Gabriel Guzman <gabe@guzman-nunez.com>
2014-09-23 22:18:37 -0400

    More unit test breakage.

--- echo_test.go
+++ echo_test.go
@@ -49,7 +49,6 @@ func TestEcho(t *testing.T) {
 	p := Echo{}
 	p.Execute(message, bot)
 	expectedReply := "this is a test of the emergency broadcast network."
-	reply := echo("echo this is a test of the emergency broadcast network.")
 	if reply != expectedReply {
 		t.Errorf("Test: '%s' does not equal '%s'", reply, expectedReply)
 	}
