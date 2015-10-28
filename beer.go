5ce060e4dc608538ab7329be390a97bf33fd51f8
Gabriel Guzman <gabriel.guzman@lightspeedpos.com>
2015-10-27 23:08:36 -0400

    Refactor beer plugin
    Don't reply to messages that come from the bot.  Make aye and nay responses a
    little more vaired. Remove some code duplication.

--- beer.go
+++ beer.go
@@ -2,6 +2,7 @@ package gobotplugin
 
 import (
 	"github.com/gabeguz/xmppbot"
+	"math/rand"
 	"strings"
 	"time"
 )
@@ -13,19 +14,26 @@ func (p Beer) Name() string {
 }
 
 func (p Beer) Execute(msg xmppbot.Message, bot xmppbot.Bot) error {
+	// Don't reply if it's the bot asking
+	if msg.From() == bot.FullName() {
+		return nil
+	}
+
+	var reply string
+
 	if strings.HasPrefix(msg.Body(), "beer?") {
-		if msg.From() != bot.FullName() {
-			reply := beer()
-			bot.Send(reply)
-		}
+		reply = beer()
 	}
 
 	if strings.HasPrefix(msg.Body(), "ビール?") {
-		if msg.From() != bot.FullName() {
-			reply := beerJapan()
-			bot.Send(reply)
-		}
+		reply = beer()
 	}
+
+	if strings.HasPrefix(msg.Body(), "맥주?") {
+		reply = beer()
+	}
+
+	bot.Send(reply)
 
 	return nil
 }
@@ -33,9 +41,13 @@ func (p Beer) Execute(msg xmppbot.Message, bot xmppbot.Bot) error {
 func beer() string {
 	now := time.Now()
 	if now.Weekday().String() == "Friday" && now.Hour() >= 16 {
-		return "proost! (beer)"
+		rand.Seed(time.Now().UnixNano())
+		yup := ayes[rand.Intn(len(ayes))]
+		return yup
 	} else {
-		return "wait for it..."
+		rand.Seed(time.Now().UnixNano())
+		nope := nays[rand.Intn(len(nays))]
+		return nope
 	}
 }
 
@@ -46,4 +58,19 @@ func beerJapan() string {
 	} else {
 		return "wait for it..."
 	}
+}
+
+var ayes = []string{
+	"proost! (beer)",
+	"(beer) 乾杯",
+	"cheers! (beer)",
+	"¡salud! (beer)",
+	"(beer) 건배",
+}
+
+var nays = []string{
+	"wait for it...",
+	"beer must wait.",
+	"(yoda) Friday, the beer drinking day is. Until four o'clock must you wait.",
+	"Really want to hit the ballmer peak eh?",
 }
